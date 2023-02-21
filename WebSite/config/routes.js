ec1bf3964778e6e22b04dadad6896b5761805653
Npitcher12 <63477243+Npitcher12@users.noreply.github.com>
2023-02-20 22:37:19 -0500

    Import checklist page and functionality

--- WebSite/config/routes.js
+++ WebSite/config/routes.js
@@ -25,6 +25,11 @@ module.exports.routes = {
   '/fetchEvents': { controller: 'SchedulePageController', action: 'fetchEvents', view: 'pages/homepage' },
   '/fetchAllEvents': { controller: 'SchedulePageController', action: 'fetchAllEvents', view: 'pages/homepage' },
   '/checklist': { view: 'pages/checklist'},
+  'GET /blogs/create': { controller: 'BlogController', action: 'blog_create_get' },
+  'GET /blogs': { view: 'blogs/index', controller: 'BlogController', action: 'blog_index' },
+  'POST /blogs': { controller: 'BlogController', action: 'blog_create_post' },
+  'GET /blogs/:id': { controller: 'BlogController', action: 'blog_details' },
+  'DELETE /blogs/:id': { controller: 'BlogController', action: 'blog_delete' },
   '/campus_resources': { view: 'pages/campus_resources'},
   '/academic_resources': { view: 'pages/academic_resources'},
 
